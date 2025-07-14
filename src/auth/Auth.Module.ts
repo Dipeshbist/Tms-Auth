8e2756adf709cbfdf02ff0d9d06f87634349e10e
Dipesh <dipeshbist150@gmail.com>
2025-07-14 16:59:41 +0545

    Modified

--- src/auth/Auth.Module.ts
+++ src/auth/Auth.Module.ts
@@ -1,25 +1,14 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '../user/User.Module';
 import { AuthController } from './Auth.Controller';
 import { AuthService } from './Auth.Service';
 import { JwtStrategy } from './Auth.Strategy';
+import { AppConfigModule } from 'src/config/AppConfigModule';
 // import { GoogleStrategy } from './Google.Strategy';
 
 @Module({
-  imports: [
-    UserModule,
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1d' },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [UserModule, ConfigModule, AppConfigModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
